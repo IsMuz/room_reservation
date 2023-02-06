a4a1448419673c0135f0c0cabf97243575b03dc4
makzy <akmuzyka@yandex.ru>
2023-02-06 16:50:17 +0300

    added exceptions, api route, router, new model, scheme

--- app/models/meeting_room.py
+++ app/models/meeting_room.py
@@ -8,4 +8,4 @@ class MeetingRoom(Base):
     # Имя переговорки должно быть не больше 100 символов,
     # уникальным и непустым.
     name = Column(String(100), unique=True, nullable=False)
-    description = Column(Text) 
+    description = Column(Text)
---
+++ app/schemas/meeting_room.py
@@ -0,0 +1,26 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field, validator
+
+
+class MeetingRoomBase(BaseModel):
+    name: Optional[str] = Field(None, min_length=1, max_length=100)
+    description: Optional[str]
+
+
+class MeetingRoomCreate(MeetingRoomBase):
+    name: str = Field(max_length=100)
+
+
+    @validator('name')
+    def name_is_not_none(cls, v):
+        if v is None:
+            raise ValueError('can not be none')
+        return v
+
+
+class MeetingRoomDB(MeetingRoomBase):
+    id: int
+
+    class Config:
+        orm_mode = True
